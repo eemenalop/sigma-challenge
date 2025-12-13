dab0f269a0808acd898039f98e15db7f7ea670c1
eemenalop <eemena26@gmail.com>
2025-12-13 13:34:32 -0400

    Add interface for pagination response.

--- src/lib/types/api.types.ts
+++ src/lib/types/api.types.ts
@@ -9,4 +9,17 @@ export interface ErrorResponse {
     details?: string[];
 }
 
-export type ApiResponse<T> = SuccessResponse<T> | ErrorResponse;
+export interface PaginationMeta{
+    currentPage: number;
+    totalPages: number;
+    totalItems: number;
+    itemsPerPage: number;
+}
+
+export interface PaginatedResponse<T>{
+    success: true;
+    data: T[];
+    meta: PaginationMeta;
+}
+
+export type ApiResponse<T> = SuccessResponse<T> | ErrorResponse | PaginatedResponse<T>;
