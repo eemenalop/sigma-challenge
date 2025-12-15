79d77025c1f8733e920d22a5148d298c460ebf48
eemenalop <eemena26@gmail.com>
2025-12-14 21:32:00 -0400

    Refactor focus-visible outline class in Pagination component

--- src/app/components/Pagination.tsx
+++ src/app/components/Pagination.tsx
@@ -70,7 +70,7 @@ export default function Pagination({
                 aria-current={page === currentPage ? 'page' : undefined}
                 className={`relative inline-flex items-center px-4 py-2 text-sm font-semibold ${
                   page === currentPage
-                    ? 'z-10 bg-indigo-500 text-white focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500'
+                    ? 'z-10 bg-indigo-500 text-white focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500'
                     : 'text-gray-200 ring-1 ring-inset ring-gray-700 hover:bg-white/5 focus:z-20 focus:outline-offset-0'
                 }`}
               >
